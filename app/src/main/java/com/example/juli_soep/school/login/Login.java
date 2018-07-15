270559ac0116cbce850fbd480f2f8f876f73a544
JuliSuprapto <gakgaul3@gmail.com>
2018-07-15 15:51:03 +0700

    update slider

--- app/src/main/java/com/example/juli_soep/school/login/Login.java
+++ app/src/main/java/com/example/juli_soep/school/login/Login.java
@@ -1,12 +1,19 @@
-package com.example.juli_soep.school;
+package com.example.juli_soep.school.login;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import com.example.juli_soep.school.main_menu.MainActivity;
+import com.example.juli_soep.school.R;
+
 public class Login extends AppCompatActivity {
+
+    Button btn_login;
 
     RelativeLayout relay_one, relay_two;
     Handler handler = new Handler();
@@ -26,5 +33,15 @@ public class Login extends AppCompatActivity {
         relay_one = (RelativeLayout)findViewById(R.id.rellay_one);
         relay_two = (RelativeLayout)findViewById(R.id.rellay_two);
         handler.postDelayed(runnable, 2000);
+
+        btn_login = (Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(Login.this, MainActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 }
